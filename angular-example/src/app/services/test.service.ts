aeb67685f56586cc7fecaedc57ae0033f8d025d8
Ruan <ruan7lg7@gmail.com>
2021-05-05 17:01:56 +0200

    Commit

--- angular-example/src/app/services/test.service.ts
+++ angular-example/src/app/services/test.service.ts
@@ -11,22 +11,39 @@ export class TestService {
 
   getRequest(url: string)
   { 
-    let myHeaders = new HttpHeaders();
-   // myHeaders = myHeaders.delete('Content-Type');
-   // myHeaders = myHeaders.set('Accept', 'text/plain');
 
+     let myHeaders = new HttpHeaders()
 console.log(myHeaders);
       return this.http.get<HttpResponse<any>>(url, {observe: 'response',  'headers': myHeaders});
   }
 
   postRequest(url: string, body: any)
   {
-    return this.http.post<HttpResponse<any>>(url, body, {observe: 'response'});
+        //Make it a "simple request" so it don't send the preflight "OPTION" request
+        //Change headers "content-type" to "plain/text" so OPTION request is not send, but only the POST is send
+        //OPTION is NOT supported by the "simplelink" module!
+    const /*let*/ myHeaders = new HttpHeaders()
+    .set("content-type", "text/plain");
+    //.delete("Accept", "application/json")
+    //.set("Accept", "text/plain")
+    //.set("Access-Control_Allow-Origin", "/*");
+
+    return this.http.post<HttpResponse<any>>(url, body, {observe: 'response', 'headers': myHeaders});
   }
 
   deleteRequest(url: string)
   {
-    return this.http.delete<HttpResponse<any>>(url, {observe: 'response'});
+    //Delete & PUT can NOT be "simple" requests ???????????
+    const myHeaders1 = new HttpHeaders()
+    .set("content-type", "text/plain");
+
+   //.delete("content-type", "text/plain");
+   // .delete("Accept", "text/plain")
+    //.set("Accept", "text/plain")
+   // .set("Access-Control_Allow-Origin", "*");
+
+console.log(myHeaders1.get("content-type"));
+    return this.http.delete<HttpResponse<any>>(url, {observe: 'response', 'headers': myHeaders1});
   }
 
   putRequest(url: string, body: any)
